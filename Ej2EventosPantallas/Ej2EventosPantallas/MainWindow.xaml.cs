912d72a1f68c49d9c70c70997c2b574314618b46
Nikypm <69223734+Nikypm@users.noreply.github.com>
2021-03-21 21:46:11 -0400

    Add files via upload
    Agregando el Ejercicio 2 de la presentación de "Eventos"

---
+++ Ej2EventosPantallas/Ej2EventosPantallas/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ej2EventosPantallas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 w2 = new Window2();
+            w2.cambio += new Window2.Cambio(CambiarNombreVentana);
+            w2.Show();
+        }
+        public void CambiarNombreVentana(string title)
+        {
+            Window1.Title = title;
+        }
+    }
+}
---
+++ Ej2EventosPantallas/Ej2EventosPantallas/Window2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ej2EventosPantallas
+{
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void Cambio(string title);
+        public event Cambio cambio;
+
+        public void CambiarNombreVentana(string title)
+        {
+            if (cambio != null)
+            {
+                cambio(title);
+            }
+        }
+
+        private void btnCambiarTittle_Click(object sender, RoutedEventArgs e)
+        {
+            cambio(txbTitle.Text);
+        }
+    }
+}
